44104edd74d570d76609be77b090d7fd1f2672bb
Sherif <cheryvmak.cs@gmail.com>
2026-01-07 17:21:33 +0100

    RAG for Personal cv

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,50 @@
 
 
-import streamlit as st
-import requests
+# import streamlit as st
+# import requests
 
+
+# # Page config
+# st.set_page_config(page_title="Personal RAG Assistant", layout="centered")
+
+# # Title & description
+# st.title("My Personal RAG Assistant")
+# st.write("Ask questions about my CV and personal documents.")
+
+# st.markdown(
+#     """
+#     **Assistant Persona:**  
+#     I answer questions strictly based on Okemakinde Sherif's personal documents  
+#     (CV, experience, education, projects).
+#     """
+# )
+
+
+# # Input box
+# question = st.text_input("Ask a question")
+
+# # Button
+# if st.button("Ask"):
+#     if question.strip() == "":
+#         st.warning("Please enter a question.")
+#     else:
+#         with st.spinner("Thinking..."):
+#             response = requests.post(
+#                 "http://127.0.0.1:8000/query",
+#                 json={"question": question}
+#             )
+
+#         if response.status_code == 200:
+#             data = response.json()
+
+#             st.subheader("Answer")
+#             st.write(data["answer"])
+#         else:
+#             st.error("Something went wrong. Please try again.")
+
+# #streamlit run streamlit_app.py
+import streamlit as st
+from rag_query import run_rag
 
 # Page config
 st.set_page_config(page_title="Personal RAG Assistant", layout="centered")
@@ -19,7 +61,6 @@ st.markdown(
     """
 )
 
-
 # Input box
 question = st.text_input("Ask a question")
 
@@ -29,17 +70,7 @@ if st.button("Ask"):
         st.warning("Please enter a question.")
     else:
         with st.spinner("Thinking..."):
-            response = requests.post(
-                "http://127.0.0.1:8000/query",
-                json={"question": question}
-            )
+            result = run_rag(question)
 
-        if response.status_code == 200:
-            data = response.json()
-
-            st.subheader("Answer")
-            st.write(data["answer"])
-        else:
-            st.error("Something went wrong. Please try again.")
-
-#streamlit run streamlit_app.py
+        st.subheader("Answer")
+        st.write(result["answer"])
